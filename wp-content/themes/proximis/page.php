2709fe6bade1ba36c3f25c6322894c16ae0b3f4a
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-06-17 17:42:49 +0200

    :art: basic styles

--- wp-content/themes/proximis/page.php
+++ wp-content/themes/proximis/page.php
@@ -4,11 +4,14 @@
 
 	<?php if ( have_posts() ) : the_post(); ?>
 
+		<?php if( function_exists('yoast_breadcrumb') ){ yoast_breadcrumb( '<p id="breadcrumbs">','</p>' ); } ?>
+
 		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-	
+
+		<div class='container-small'><?php the_content(); ?></div>
+		
 	<?php else : ?>
-				
+				 
 		<h1>404</h1>
 
 	<?php endif; ?>
