6ece535c5779c0188b919b8620290699c19880e2
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-07-02 18:44:15 +0200

    🔥 conflict code clear

--- wp-content/themes/proximis/js/main.js
+++ wp-content/themes/proximis/js/main.js
@@ -23348,9 +23348,9 @@ var query = function query(selector, context) {
   !*** ./wp-content/themes/proximis/src/scss/main.scss ***!
   \*******************************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\n    }\n    ^\n      Invalid CSS after \"    }\": expected \"}\", was \"<<<<<<< HEAD .ref {\"\n      in /Users/adrienlementheour/Travail/proximis/proximis/wp-content/themes/proximis/src/scss/pages/_home.scss (line 148, column 6)\n    at runLoaders (/Users/adrienlementheour/Travail/proximis/proximis/node_modules/webpack/lib/NormalModule.js:302:20)\n    at /Users/adrienlementheour/Travail/proximis/proximis/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/adrienlementheour/Travail/proximis/proximis/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/adrienlementheour/Travail/proximis/proximis/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.render [as callback] (/Users/adrienlementheour/Travail/proximis/proximis/node_modules/sass-loader/lib/loader.js:52:13)\n    at Object.done [as callback] (/Users/adrienlementheour/Travail/proximis/proximis/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Users/adrienlementheour/Travail/proximis/proximis/node_modules/node-sass/lib/index.js:294:32)");
+// extracted by mini-css-extract-plugin
 
 /***/ })
 
