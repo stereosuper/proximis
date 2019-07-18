cd9f3cd7ab3d865605ff7c9d7cc4ab6a2aa6cf82
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-07-18 20:04:04 +0200

    :art: post meta

--- wp-content/themes/proximis/single.php
+++ wp-content/themes/proximis/single.php
@@ -4,15 +4,28 @@
 
 	<?php if ( have_posts() ) : the_post(); ?>
 
-		<h1><?php the_title(); ?></h1>
-		<time><?php echo get_the_date(); ?></time>
+		<?php if( function_exists('yoast_breadcrumb') ){ yoast_breadcrumb( '<p id="breadcrumbs" class="breadcrumbs">','</p>' ); } ?>
 
-		<?php the_content(); ?>
+		<h1 class='blog-title'><?php the_title(); ?></h1>
+		<p class='post-meta'>
+			<time><?php echo get_the_date(); ?></time>
+			<?php echo do_shortcode('[rt_reading_time postfix="min"]'); ?>
+			<span class='cats'>
+				<?php $cats = get_the_category(); if( $cats ) :
+					$count = 0;
+					foreach( $cats as $cat ) :
+						$count ++;
+						if( $count > 1 ) echo ', ';
+						echo '<a href="' . get_category_link( $cat->term_id ) . '">' . $cat->cat_name . '</a>';
+					endforeach;
+				endif; ?>
+			</span>
+		</p>
+		
+		<?php the_post_thumbnail('full'); ?>
 
-	<?php else : ?>
-				
-		<h1>404</h1>
-
+		<div class='container-tiny'><?php the_content(); ?></div>
+		
 	<?php endif; ?>
 
 </article>
