35af01c3cbc544e3d6270a2952999d34ce0dda90
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-07-19 16:20:34 +0200

    :art: newsletter

--- wp-content/themes/proximis/single.php
+++ wp-content/themes/proximis/single.php
@@ -38,6 +38,14 @@
 					<?php echo $author; ?>
 				</a>
 			</p>
+
+			<div class='newsletter'>
+				<div class='text'>
+					<h2><?php the_field('title_news', 'options'); ?></h2>
+					<p><?php the_field('text_news', 'options'); ?></p>
+				</div>
+				<?php echo do_shortcode('[contact-form-7 id="250" title="Newsletter"]'); ?>
+			</div>
 		</div>
 		
 	<?php endif; ?>
