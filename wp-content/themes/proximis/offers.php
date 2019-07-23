f59e7e211df93ba0fb9436fdef9436bdfe52ccac
Elisabeth Hamel <zaza@MacBook-Pro-de-Elisabeth.local>
2019-07-23 12:31:15 +0200

    :art: offers template

--- wp-content/themes/proximis/offers.php
+++ wp-content/themes/proximis/offers.php
@@ -16,6 +16,12 @@ get_header(); ?>
 
 			<?php the_content(); ?>
 
+			<?php if( have_rows('travelguides') ) : ?>
+				<?php while( have_rows('travelguides') ) : the_row(); ?>
+					<?php echo wp_get_attachment_image(get_sub_field('img')); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+
 			<?php if( have_rows('values') ) : ?>
 				<?php while( have_rows('values') ) : the_row(); ?>
 					<div>
@@ -38,14 +44,47 @@ get_header(); ?>
 
 	<?php if( have_rows('offers') ) : ?>
 		<?php while( have_rows('offers') ) : the_row(); ?>
-			<section>
+			<section id='offers'>
 				<div class='container'>
 					<h2><?php the_sub_field('title'); ?></h2>
+
+					<ul>
+						<?php if( have_rows('links') ) : ?>
+							<?php while( have_rows('links') ) : the_row(); ?>
+								<li>
+									<?php $link = get_sub_field('link'); ?>
+									<a href='<?php echo $link['url']; ?>'><?php echo $link['title']; ?>
+										<span><?php the_sub_field('job_type'); ?></span>
+										<span><?php the_sub_field('place'); ?></span>
+									</a>
+								</li>
+							<?php endwhile; ?>
+						<?php endif; ?>
+					</ul>
 
 					<?php the_sub_field('text'); ?>
 				</div>
 			</section>
 		<?php endwhile; ?>
+	<?php endif; ?>
+
+	<?php if( have_rows('quotes') ) : ?>
+		<div class='container'>
+			<ul>
+				<?php while( have_rows('quotes') ) : the_row(); ?>
+					<li>
+						<blockquote>
+							<?php the_sub_field('text'); ?>
+						</blockquote>
+						<p>
+							<?php echo wp_get_attachment_image(get_sub_field('img')); ?>
+							<?php the_sub_field('name'); ?>
+							<?php the_sub_field('job'); ?>
+						</p>
+					</li>
+				<?php endwhile; ?>
+			</ul>
+		</div>
 	<?php endif; ?>
 		
 <?php endif; ?>
