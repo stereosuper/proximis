b89cf8789bafa667a633038ad73d757a1d12210f
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-08-07 16:18:40 +0200

    💄 partners

--- wp-content/themes/proximis/partners.php
+++ wp-content/themes/proximis/partners.php
@@ -12,36 +12,40 @@ get_header(); ?>
 		<?php if( function_exists('yoast_breadcrumb') ){ yoast_breadcrumb( '<p id="breadcrumbs" class="breadcrumbs">','</p>' ); } ?>
 
 		<div class='container-medium'>
-			<h1><?php the_title(); ?></h1>
+			<h1 class='small-margin-bottom'><?php the_title(); ?></h1>
 			<?php the_content(); ?>
+		</div>
 
-			<?php if( have_rows('partners') ) : ?>
-				<ul>
-					<?php while( have_rows('partners') ) : the_row(); ?>
-						<li>
+		<?php if( have_rows('partners') ) : ?>
+			<ul class="partners">
+				<?php while( have_rows('partners') ) : the_row(); ?>
+					<li>
+						<div class="img">
+							<?php echo wp_get_attachment_image(get_sub_field('logo')); ?>
+						</div>
+						<div class="text">
 							<?php $link = get_sub_field('link'); ?>
-							<?php echo wp_get_attachment_image(get_sub_field('name')); ?>
+							<div class="partner-name"><?php the_sub_field('name'); ?></div>
 							<?php if( $link ) : ?>
 								<a href='<?php echo $link['url']; ?>'><?php echo $link['title']; ?></a>
 							<?php endif; ?>
-							<?php the_sub_field('name'); ?>
-							<?php the_sub_field('text'); ?>
+							<p><?php the_sub_field('text'); ?></p>
 							<?php if( $link ) : ?>
 								<a href='<?php echo $link['url']; ?>' class='link'>
 									<span><?php _e('Learn more', 'proximis'); ?></span><i></i>
 								</a>
 							<?php endif; ?>
-						</li>
-					<?php endwhile; ?>
-				</ul>
-			<?php endif; ?>
+						</div>
+					</li>
+				<?php endwhile; ?>
+			</ul>
+		<?php endif; ?>
 
-			<?php $form = get_field('form'); if( $form ) : ?>
-				<h2><?php echo $form['title']; ?></h2>
-				<?php echo $form['text']; ?>
-				<?php echo do_shortcode($form['shortcode']); ?>
-			<?php endif; ?>
-		</div>
+		<?php $form = get_field('form'); if( $form ) : ?>
+			<h2><?php echo $form['title']; ?></h2>
+			<?php echo $form['text']; ?>
+			<?php echo do_shortcode($form['shortcode']); ?>
+		<?php endif; ?>
 		
 	<?php endif; ?>
 
