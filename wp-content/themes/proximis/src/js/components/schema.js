86ecd19953d8839343f40d1090c0b154a45d9766
Elisabeth Hamel <zaza@MacBook-Pro-de-Elisabeth.local>
2019-08-09 10:57:59 +0200

    :bug: fix schema hover

--- wp-content/themes/proximis/src/js/components/schema.js
+++ wp-content/themes/proximis/src/js/components/schema.js
@@ -1,5 +1,5 @@
 import { forEach } from '@stereorepo/sac';
-import { TimelineMax, TweenMax } from 'gsap';
+import { Linear, TweenMax } from 'gsap';
 
 const schemaHandler = () => {
     const schema = document.getElementById('schema');
@@ -16,43 +16,64 @@ const schemaHandler = () => {
     const texts = schema.querySelectorAll('.btn-text');
     const btns = schema.querySelectorAll('.schema-btn');
     let desc, bbox;
-    const centerRotation = TweenMax.to(int, 50, {rotation: '-=360', transformOrigin: '50% 50%', repeat: -1, ease: Linear.easeNone, paused: true});
-    const schemaRect = schema.getBoundingClientRect();
-
+    const centerRotation = TweenMax.to(int, 50, {
+        rotation: '-=360',
+        transformOrigin: '50% 50%',
+        repeat: -1,
+        ease: Linear.easeNone,
+        paused: true
+    });
+    let schemaRect = {};
 
     const animSchema = () => {
-        TweenMax.to(indic, 0.3, {opacity: 1});
-        TweenMax.to(dot, 0.3, {opacity: 1, delay: 0.2});
-        TweenMax.to(bg, 0.3, {fill: '#44BAD1', delay: 0.6});
-        TweenMax.to(circle, 0.3, {stroke: '#44BAD1', strokeWidth: 2, strokeDasharray: '1, 0', delay: 0.6});
-        TweenMax.to([bgLogo, texts], 0.3, {fill: '#fff', delay: 0.6});
-        TweenMax.to(ext, 2, {rotation: "+=120", transformOrigin: "50% 50%", onComplete: animSchema, delay: 5});
-        TweenMax.to(indic, 0.3, {opacity: 0, delay: 5});
-        TweenMax.to(dot, 0.3, {opacity: 0, delay: 4.6});
-        TweenMax.to(bg, 0.3, {fill: '#fff', delay: 4.2});
-        TweenMax.to([bgLogo, texts], 0.3, {fill: '#18162B', delay: 4.2});
-        TweenMax.to(circle, 0.3, {stroke: '#18162B', strokeWidth: 1, strokeDasharray: '2, 2', delay: 4.2});
-    }
+        TweenMax.to(indic, 0.3, { opacity: 1 });
+        TweenMax.to(dot, 0.3, { opacity: 1, delay: 0.2 });
+        TweenMax.to(bg, 0.3, { fill: '#44BAD1', delay: 0.6 });
+        TweenMax.to(circle, 0.3, {
+            stroke: '#44BAD1',
+            strokeWidth: 2,
+            strokeDasharray: '1, 0',
+            delay: 0.6
+        });
+        TweenMax.to([bgLogo, texts], 0.3, { fill: '#fff', delay: 0.6 });
+        TweenMax.to(ext, 2, {
+            rotation: '+=120',
+            transformOrigin: '50% 50%',
+            onComplete: animSchema,
+            delay: 5
+        });
+        TweenMax.to(indic, 0.3, { opacity: 0, delay: 5 });
+        TweenMax.to(dot, 0.3, { opacity: 0, delay: 4.6 });
+        TweenMax.to(bg, 0.3, { fill: '#fff', delay: 4.2 });
+        TweenMax.to([bgLogo, texts], 0.3, { fill: '#18162B', delay: 4.2 });
+        TweenMax.to(circle, 0.3, {
+            stroke: '#18162B',
+            strokeWidth: 1,
+            strokeDasharray: '2, 2',
+            delay: 4.2
+        });
+    };
 
     centerRotation.play();
-    TweenMax.set(ext, {rotation: 60, transformOrigin: '50% 50%'});
+    TweenMax.set(ext, { rotation: 60, transformOrigin: '50% 50%' });
     animSchema();
 
     forEach(btns, btn => {
         btn.addEventListener('mouseenter', () => {
             centerRotation.pause();
             desc = document.getElementById(btn.id + '-desc');
-            if( !desc ) return;
+            if (!desc) return;
+            schemaRect = schema.getBoundingClientRect();
             bbox = btn.getBoundingClientRect();
-            desc.style.top = (bbox.y - schemaRect.y + bbox.height + 3) + 'px';
-            desc.style.left = (bbox.x - schemaRect.x - 20) + 'px';
+            desc.style.top = bbox.y - schemaRect.y + bbox.height + 3 + 'px';
+            desc.style.left = bbox.x - schemaRect.x - 20 + 'px';
             desc.classList.add('on');
         });
 
         btn.addEventListener('mouseleave', () => {
             centerRotation.resume();
             desc = document.getElementById(btn.id + '-desc');
-            if( !desc ) return;
+            if (!desc) return;
             desc.classList.remove('on');
         });
     });
