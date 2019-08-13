b7b48b24fcd579d25ef2972180e63ca6ec38afe2
Elisabeth Hamel <zaza@MacBook-Pro-de-Elisabeth.local>
2019-08-13 17:49:52 +0200

    :art: mission section 1

--- wp-content/themes/proximis/mission.php
+++ wp-content/themes/proximis/mission.php
@@ -9,25 +9,42 @@ get_header(); ?>
 
 	<?php get_template_part('includes/header-page'); ?>
 
-	<div class='container'>
+	<div class='container mission'>
 		<?php if( have_rows('section1') ) : while( have_rows('section1') ) : the_row(); ?>
-			<section>
-				<h2><?php the_sub_field('title'); ?></h2>
-				<?php the_sub_field('text'); ?>
-				<?php the_sub_field('columns'); ?>
-				<?php if( have_rows('examples') ) : while( have_rows('examples') ) : the_row(); ?>
-					<div>
-						<?php the_sub_field('title'); ?>
-						<?php the_sub_field('text'); ?>
+			<section class='mission-section'>
+				<div class='container-tiny'>
+					<h2><?php the_sub_field('title'); ?></h2>
+					<div class='big-text'><?php the_sub_field('text'); ?></div>
+					<div class='cols-text'><?php the_sub_field('columns'); ?></div>
+				</div>
+				<?php if( have_rows('examples') ) : ?>
+					<div class='examples'>
+						<?php while( have_rows('examples') ) : the_row(); ?>
+							<div class='example'>
+								<h3><?php the_sub_field('title'); ?></h3>
+								<p><?php the_sub_field('text'); ?></p>
+							</div>
+						<?php endwhile; ?>
 					</div>
-				<?php endwhile; endif; ?>
-				<?php the_sub_field('text2'); ?>
-				<?php if( have_rows('links') ) : while( have_rows('links') ) : the_row(); ?>
-					<div>
-						<?php the_sub_field('title'); ?>
-						<?php the_sub_field('cat'); ?>
-					</div>
-				<?php endwhile; endif; ?>
+				<?php endif; ?>
+				<div class='container-tiny'>
+					<?php the_sub_field('text2'); ?>
+					<?php if( have_rows('links') ) : ?>
+						<p class='mission-title'><?php _e('For further information...', 'proximis'); ?></p>
+						<div class='mission-links'>
+							<?php while( have_rows('links') ) : the_row(); ?>
+								<?php $link = get_sub_field('link'); ?>
+								<a href='<?php echo $link['url']; ?>' class='mission-link'>
+									<span class='sup-title'><?php the_sub_field('cat'); ?></span>
+									<span class='title'><?php the_sub_field('title'); ?></span>
+									<span class='link small'>
+										<span><?php echo $link['title']; ?></span><i></i>
+									</span>
+								</a>
+							<?php endwhile; ?>
+						</div>
+					<?php endif; ?>
+				</div>
 			</section>
 		<?php endwhile; endif; ?>
 	</div>
