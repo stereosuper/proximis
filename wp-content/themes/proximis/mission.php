26d851c834327d86439e4fd847577d55415f81fd
ElisabethStereosuper <elisabeth@stereosuper.fr>
2019-08-15 20:58:32 +0200

    :art: mission articles + footer

--- wp-content/themes/proximis/mission.php
+++ wp-content/themes/proximis/mission.php
@@ -315,13 +315,28 @@ get_header(); ?>
 				</div>
 				<div class='container-medium'>
 					<?php if( have_rows('articles') ) : ?>
-						<?php while( have_rows('articles') ) : the_row(); ?>
-							<div>
-								<time><?php the_sub_field('date'); ?></time>
-								<h3><?php the_sub_field('titre'); ?></h3>
-								<p><?php the_sub_field('text'); ?></p>
-							</div>
-						<?php endwhile; ?>
+						<div class='mission-articles'>
+							<?php while( have_rows('articles') ) : the_row(); ?>
+								<div class='mission-article'>
+									<?php
+										$linkSite = get_sub_field('link_site');
+										$link = get_sub_field('link');
+									?>
+									<a href='<?php echo $linkSite['url']; ?>'>
+										<?php echo wp_get_attachment_image(get_sub_field('logo'), 'medium'); ?>
+									</a>
+									<div class='meta'>
+										<a href='<?php echo $linkSite['url']; ?>'><?php echo $linkSite['title']; ?></a>
+										<time><?php the_sub_field('date'); ?></time>
+									</div>
+									<h3><a href='<?php echo $link['url']; ?>'><?php the_sub_field('titre'); ?></a></h3>
+									<p><?php the_sub_field('text'); ?></p>
+									<a href='<?php echo $link['url']; ?>' class='link small'>
+										<span><?php echo $link['title']; ?></span><i></i>
+									</a>
+								</div>
+							<?php endwhile; ?>
+						</div>
 					<?php endif; ?>
 				</div>
 				<div class='container-tiny'>
