96e7ecad215d999bccf9d9462299a58354d8f18b
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-02-03 16:58:13 +0100

    ✨ gtag newsletter blog

--- wp-content/themes/proximis/src/js/components/newsletter.js
+++ wp-content/themes/proximis/src/js/components/newsletter.js
@@ -2,11 +2,15 @@ import { forEach, query } from '@stereorepo/sac';
 
 const newsHandler = () => {
     const newsletters = query({ selector: '.newsletter' });
-    let input, email;
+    let form, input, email;
 
     if (!newsletters.length) return;
 
     forEach(newsletters, elt => {
+        [form] = query({ selector: '.wpcf7-form' });
+
+        if (!form) return;
+
         [input] = query({
             selector: '.wpcf7-email',
             ctx: elt
@@ -22,6 +26,13 @@ const newsHandler = () => {
             });
             input.addEventListener('blur', () => {
                 if (!input.value) email.classList.remove('on');
+            });
+        }
+
+        if (form.classList.contains('sent')) {
+            gtag('event', 'newsletter', {
+                event_category: 'inscription',
+                event_label: 'blog'
             });
         }
     });
