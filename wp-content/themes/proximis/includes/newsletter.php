bb441714f5b3efc691a05cdac95385276410f9eb
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-02-17 15:30:01 +0100

    🐛 fix newsletter condition

--- wp-content/themes/proximis/includes/newsletter.php
+++ wp-content/themes/proximis/includes/newsletter.php
@@ -1,7 +1,9 @@
-<div class='newsletter'>
-	<div class='text'>
-		<h2><?php the_field('title_news', 'options'); ?></h2>
-		<p><?php the_field('text_news', 'options'); ?></p>
+<?php if( get_field('shortcode_news', 'options') ): ?>
+	<div class='newsletter'>
+		<div class='text'>
+			<h2><?php the_field('title_news', 'options'); ?></h2>
+			<p><?php the_field('text_news', 'options'); ?></p>
+		</div>
+		<?php echo do_shortcode(get_field('shortcode_news', 'options')); ?>
 	</div>
-	<?php echo do_shortcode(get_field('shortcode_news', 'options')); ?>
-</div>
+<?php endif; ?>
